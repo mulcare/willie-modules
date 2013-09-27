6fe1210228ef2ada4f2fc0100212267095c42d19
Ross Mulcare <ross.mulcare@gmail.com>
2013-09-27 12:30:53 -0400

    initial google image search module init

---
+++ gis.py
@@ -0,0 +1,44 @@
+"""
+gis.py - Google Image Search Willie module
+Ross Mulcare - rossmulcare.net
+Unlicensed - Use, modify, sell; I don't care.
+-
+Modified version of search.py 
+by Sean B. Palmer / Edward Powell
+"""
+
+from willie import module, web
+import json
+
+def google_ajax(query):
+    uri = 'http://ajax.googleapis.com/ajax/services/search/images'
+    args = '?v=1.0&safe=off&q=' + web.quote(query)
+    bytes = web.get(uri + args)
+    return json.loads(bytes)
+
+def google_search(query):
+    result = google_ajax(query)
+    height = result['responseData']['results'][0]['height']
+    width = result['responseData']['results'][0]['width']
+    url = result['responseData']['results'][0]['url']
+    size = height + 'x' + width
+    try:
+	 return url + ' \x035[' + size + ']\x03'
+    except IndexError:
+        return None
+    except TypeError:
+        return False
+
+@module.commands('gis', 'img')
+@module.example('.gis french bulldog')
+def gis(bot, trigger):
+    query = trigger.group(2)
+    if not query:
+        return bot.reply('.gis what?')
+    uri = google_search(query)
+    if uri:
+        bot.reply(uri)
+    elif uri is False:
+        bot.reply("Problem getting data from Google.")
+    else:
+        bot.reply("No results found for '%s'." % query)
